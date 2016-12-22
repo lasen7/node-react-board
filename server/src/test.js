0ffa8e5ea762f29555e677871cc4db2d6c6eff5e
lasen7 <lasen7@naver.com>
2016-12-23 01:38:34 +0900

    Create react app

--- server/src/test.js
+++ server/src/test.js
@@ -1,3 +1,30 @@
 //import uuid from 'uuid/v1';
-const uuid = require('uuid/v1');
-console.log(uuid());
+// const uuid = require('uuid/v1');
+// console.log(uuid());
+
+// const foo = () => {
+//   console.log('foo');
+//   try {
+//     throw Error('foo error');
+//   } catch (err) {
+//     //return Promise.reject(err);
+//   }
+//   return Promise.resolve();
+// };
+
+// const goo = () => {
+//   console.log('goo');
+//   return Promise.resolve(5);
+// };
+
+// foo()
+//   .then(() => {
+//     return goo();
+//   })
+//   .then(result => {
+//     console.log('result: ', result);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
